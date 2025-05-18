bea5c545114c74cd73a0cb14767b9dc93b0ee676
ashish-s-eq <designer.ashish@hotmail.com>
2025-05-18 16:29:56 +0530

    18-05.2025

--- src/Mains.jsx
+++ src/Mains.jsx
@@ -12,6 +12,19 @@ function Mains() {
                     <h4 className="fw-bold mb-2">I am Ashish</h4>
                     <h1>Next-Level Web Developer.</h1>
                     <p className="mx-w400">I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people</p>
+                    <div className="mt-5 d-flex align-item-center gap-4">
+                        <div>
+                            <a href="" className="download d-flex align-items-center text-decoration-none">Download CV <i className="ms-2 fa fa-download"></i></a>
+                        </div>
+                        <div className="d-flex">
+                            <ul className="d-flex align-items-center gap-3 list-unstyled p-0 m-0">
+                                <li className="overflow-hidden"><i className="fa fa-twitter"></i></li>
+                                <li className="overflow-hidden"><i className="fa fa-facebook"></i></li>
+                                <li className="overflow-hidden"><i className="fa fa-instagram"></i></li>
+                                <li className="overflow-hidden"><i className="fa fa-github"></i></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <div className="profile">
                     <div className="image-container">
