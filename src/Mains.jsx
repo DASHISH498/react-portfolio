7cfabecd35e08d0469ca2542d55ca49499af1110
Ashish Kumar Soni <106304040+DASHISH498@users.noreply.github.com>
2025-05-25 18:05:18 +0530

    My Quality Service section added

--- src/Mains.jsx
+++ src/Mains.jsx
@@ -2,38 +2,110 @@ import React from 'react'
 
 function Mains() {
   return (
-    <section className="mains-section overflow-hidden">
-        <div className="intro-text">
-            <svg viewBox="0 0 1320 300" class="overflow-hidden"><text x="50%" y="50%" text-anchor="middle" class="animate-stroke">HI</text></svg>
-        </div>
-        <div className="container">
-            <div className="column-group">
-                <div>
-                    <h4 className="fw-bold mb-2">I am Ashish</h4>
-                    <h1>Next-Level Web Developer.</h1>
-                    <p className="mx-w400">I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people</p>
-                    <div className="mt-5 d-flex align-item-center gap-4">
-                        <div>
-                            <a href="" className="download d-flex align-items-center text-decoration-none">Download CV <i className="ms-2 fa fa-download"></i></a>
-                        </div>
-                        <div className="d-flex">
-                            <ul className="d-flex align-items-center gap-3 list-unstyled p-0 m-0">
-                                <li className="overflow-hidden"><i className="fa fa-twitter"></i></li>
-                                <li className="overflow-hidden"><i className="fa fa-facebook"></i></li>
-                                <li className="overflow-hidden"><i className="fa fa-instagram"></i></li>
-                                <li className="overflow-hidden"><i className="fa fa-github"></i></li>
-                            </ul>
+    <>
+        <section className="mains-section overflow-hidden">
+            <div className="intro-text">
+                <svg viewBox="0 0 1320 300" class="overflow-hidden"><text x="50%" y="50%" text-anchor="middle" class="animate-stroke">HI</text></svg>
+            </div>
+            <div className="container">
+                <div className="column-group">
+                    <div>
+                        <h4 className="fw-bold mb-2">I am Ashish</h4>
+                        <h1>Next-Level Web Developer.</h1>
+                        <p className="max-w-400px">I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people</p>
+                        <div className="mt-5 d-flex align-item-center gap-4">
+                            <div>
+                                <a href="" className="download d-flex align-items-center text-decoration-none">Download CV <i className="ms-2 fa fa-download"></i></a>
+                            </div>
+                            <div className="d-flex">
+                                <ul className="d-flex align-items-center gap-3 list-unstyled p-0 m-0">
+                                    <li className="overflow-hidden"><i className="fa fa-twitter"></i></li>
+                                    <li className="overflow-hidden"><i className="fa fa-facebook"></i></li>
+                                    <li className="overflow-hidden"><i className="fa fa-instagram"></i></li>
+                                    <li className="overflow-hidden"><i className="fa fa-github"></i></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="profile">
-                    <div className="image-container">
-                        <img src="https://dashishsoni.in/assets/images/profile.jpg" alt="" />
+                    <div className="profile">
+                        <div className="image-container">
+                            <img src="https://dashishsoni.in/assets/images/profile.jpg" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+            <div className="funfact-area mt-5 mb-5">
+                <div className="container">
+                    <ul className="list-unstyled flex flex-wrap justify-between gap-3">
+                        <li className="flex flex-wrap items-center">
+                            <span className="d-block text-6xl">6.5</span>
+                            <h5 className="font-light max-w-40 ms-3">Year of Experience</h5>
+                        </li>
+                        <li className="flex flex-wrap items-center">
+                            <span className="d-block text-6xl">6</span>
+                            <h5 className="font-light max-w-40 ms-3">Project Completed</h5>
+                        </li>
+                        <li className="flex flex-wrap items-center">
+                            <span className="d-block text-6xl">400+</span>
+                            <h5 className="font-light max-w-40 ms-3">Happy Client</h5>
+                        </li>
+                        <li className="flex flex-wrap items-center">
+                            <span className="d-block text-6xl">4.8</span>
+                            <h5 className="font-light max-w-40 ms-3">Rating</h5>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section id="service">
+            <div className="container">
+                <div className="service-padding">
+                    <div className="heading text-center flex flex-col items-center">
+                        <h2 className="animate__animated animate__bounce text-transparent text-5xl font-bold leading-tight">My Quality Service</h2>
+                        <p className="m-0 max-w-700px">We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</p>
+                    </div>
+                    <div className="service-weight relative">
+                        <div className="service-item px-15px lg:px-30px border-b border-body-color dark:border-seondary-color relative z-10 group wow fadeInUp">
+                            <a href="" className="no-underline text-white flex items-center flex-wrap py-4 gap-15">
+                                <span className="left flex flex-wrap gap-4 items-center">
+                                    <b className="service-sl-num text-primary-color group-hover:text-white transition-all duration-300">01</b>
+                                    <b className="text-xl md:text-size-25 lg:text-3xl">Web Devlopment</b>
+                                </span>
+                                <span className="text-primary-color-light text-white max-w-500px">Ensure your website looks great on any device,with layouts that adapt to different screen sizes seamlessly.</span>
+                            </a>
+                        </div>
+                        <div className="service-item px-15px lg:px-30px border-b border-body-color dark:border-seondary-color relative z-10 group wow fadeInUp">
+                            <a href="" className="no-underline text-white flex items-center flex-wrap py-4 gap-15">
+                                <span className="left flex flex-wrap gap-4 items-center">
+                                    <b className="service-sl-num text-primary-color group-hover:text-white transition-all duration-300">02</b>
+                                    <b className="text-xl md:text-size-25 lg:text-3xl">Responsive Design</b>
+                                </span>
+                                <span className="text-primary-color-light text-white max-w-500px">Ensure your website looks great on any device,with layouts that adapt to different screen sizes seamlessly.</span>
+                            </a>
+                        </div>
+                        <div className="service-item px-15px lg:px-30px border-b border-body-color dark:border-seondary-color relative z-10 group wow fadeInUp">
+                            <a href="" className="no-underline text-white flex items-center flex-wrap py-4 gap-15">
+                                <span className="left flex flex-wrap gap-4 items-center">
+                                    <b className="service-sl-num text-primary-color group-hover:text-white transition-all duration-300">03</b>
+                                    <b className="text-xl md:text-size-25 lg:text-3xl">Clean Code</b>
+                                </span>
+                                <span className="text-primary-color-light text-white max-w-500px">Ensure your website looks great on any device,with layouts that adapt to different screen sizes seamlessly.</span>
+                            </a>
+                        </div>
+                        <div className="service-item px-15px lg:px-30px border-b border-body-color dark:border-seondary-color relative z-10 group wow fadeInUp">
+                            <a href="" className="no-underline text-white flex items-center flex-wrap py-4 gap-15">
+                                <span className="left flex flex-wrap gap-4 items-center">
+                                    <b className="service-sl-num text-primary-color group-hover:text-white transition-all duration-300">04</b>
+                                    <b className="text-xl md:text-size-25 lg:text-3xl">UI/UX Design</b>
+                                </span>
+                                <span className="text-primary-color-light text-white max-w-500px">Ensure your website looks great on any device,with layouts that adapt to different screen sizes seamlessly.</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.scss'
 import App from './App.jsx'
 import Header from './Header.jsx'
 import Mains from './Mains.jsx'
+import 'animate.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
